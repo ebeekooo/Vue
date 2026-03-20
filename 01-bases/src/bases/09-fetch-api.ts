2a950b33b531b6e090b07acc671e36ec6c029910
Edisbel Camargo <lebside2701@gmail.com>
2026-03-20 11:56:37 -0500

    Agregando ejercicio y archivos usando APIs con Typescript

--- 01-bases/src/bases/09-fetch-api.ts
+++ 01-bases/src/bases/09-fetch-api.ts
@@ -1,8 +1,10 @@
-const apikey = "R7mkIxy3wzEkzWFP4zkIYgm0EO5jhvWx";
+import type { GIF } from "../interfaces/gif-response";
 
-fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apikey}`)
+const apiKey = "R7mkIxy3wzEkzWFP4zkIYgm0EO5jhvWx";
+
+fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
   .then((resp) => resp.json())
-  .then((body) => {
-    console.log(body.data.images.downsizedStill);
+  .then((body: GIF) => {
+    console.log(body.data.images.downsized_medium.url);
   })
   .catch((err) => console.info(err));
