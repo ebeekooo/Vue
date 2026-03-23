39c5de8a7ca1ef6d91828b60189f84c1c60b5351
Edisbel Camargo <lebside2701@gmail.com>
2026-03-23 15:53:57 -0500

    Agregando scroll automatico

--- 03-indecision-app/src/components/chat/chatMessages.vue
+++ 03-indecision-app/src/components/chat/chatMessages.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-1 overflow-y-auto p-4">
+  <div ref="chatRef" class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
       <!-- Example Message -->
@@ -16,9 +16,20 @@
 <script setup lang="ts">
 import type { chatMessage } from '@/interfaces/chat-msg-interface';
 import ChatBubble from './chatBubble.vue';
+import { ref, watch } from 'vue';
 
 interface Props {
   messages: chatMessage[];
 }
-defineProps<Props>();
+const { messages } = defineProps<Props>();
+
+const chatRef = ref<HTMLDivElement | null>(null);
+watch(messages, () => {
+  setTimeout(() => {
+    chatRef.value?.scrollTo({
+      top: chatRef.value.scrollHeight,
+      behavior: 'smooth',
+    });
+  }, 100);
+});
 </script>
