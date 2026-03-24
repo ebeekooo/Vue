9dc2838d0223097af45a5af56650b9e32abbee54
Edisbel Camargo <lebside2701@gmail.com>
2026-03-24 13:18:35 -0500

    Agregando testing al componente messageBox

--- 03-indecision-app/tests/chat/messageBox.test.ts
+++ 03-indecision-app/tests/chat/messageBox.test.ts
@@ -1,3 +1,12 @@
+import MessageBox from "@/components/chat/messageBox.vue";
+import { mount } from "@vue/test-utils";
+
 describe("<messageBox>", () => {
-  test("renders input and output elements correctly", () => {});
+  test("renders input and output elements correctly", () => {
+    const wrapper = mount(MessageBox);
+
+    expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper.find('input[type="text"]').exists()).toBe(true);
+    expect(wrapper.find("button").exists()).toBe(true);
+  });
 });
