4542976cd80fad24cfca9e98ac39a4d2fac8b9dc
Edisbel Camargo <lebside2701@gmail.com>
2026-03-24 10:25:31 -0500

    Agregando testing con snapshot

--- 03-indecision-app/tests/components/counterApp.test.ts
+++ 03-indecision-app/tests/components/counterApp.test.ts
@@ -1,12 +1,22 @@
 import { mount } from "@vue/test-utils";
 import counter from "@/components/counterApp.vue";
-describe("Testing counte", () => {
+describe("Testing counter", () => {
   test("shouls match snapshot", () => {
     const wrapper = mount(counter, {
       props: {
         value: 5,
       },
     });
-    console.log(wrapper.html());
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  test("renders the counter values correctly", () => {
+    const wrapper = mount(counter, {
+      props: {
+        value: 5,
+      },
+    });
+
+    expect(wrapper.find("h3"));
   });
 });
