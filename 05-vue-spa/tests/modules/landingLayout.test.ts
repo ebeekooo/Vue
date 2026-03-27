4170de26b5b992fc352cbeaa4f7d33e1f6e97ee0
Edisbel Camargo <lebside2701@gmail.com>
2026-03-27 10:49:23 -0500

    Agregando testing al landingLayout

---
+++ 05-vue-spa/tests/modules/landingLayout.test.ts
@@ -0,0 +1,26 @@
+import { describe, test, expect } from 'vitest';
+import LandingLayout from '../../src/modules/landing/layouts/landingLayout.vue';
+import router from '../../src/router/index';
+import { shallowMount } from '@vue/test-utils';
+import { RouterView } from 'vue-router';
+
+describe('<LandingLayout />', () => {
+  test('should be render correctly', () => {
+    const wrapper = shallowMount(LandingLayout, {
+      global: {
+        plugins: [router],
+      },
+    });
+
+    expect(wrapper.find('header').exists()).toBe(true);
+    expect(wrapper.find('main').exists()).toBe(true);
+    expect(wrapper.find('footer').exists()).toBe(true);
+    expect(wrapper.find('footer').html()).toContain('Acme Corporation');
+    expect(wrapper.find('footer').html()).toContain(
+      `${new Date().getFullYear()} Acme Corporation. Derechos reservados`,
+    );
+
+    expect(wrapper.findComponent({ name: 'RouterView' }).exists()).toBe(true);
+    expect(wrapper.findComponent(RouterView).exists()).toBe(true);
+  });
+});
