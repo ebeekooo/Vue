7c76863997de05215bf409e56caa6a5791c2173e
Edisbel Camargo <lebside2701@gmail.com>
2026-04-07 22:09:05 -0500

    Agregando FabButtom

--- 06-projects-app/src/modules/common/components/FavBotton.vue
+++ 06-projects-app/src/modules/common/components/FavBotton.vue
@@ -20,3 +20,25 @@
     </svg>
   </button>
 </template>
+<script setup lang="ts">
+interface Props {
+  position?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+}
+withDefaults(defineProps<Props>(), {
+  position: 'bottom-right',
+});
+</script>
+<style scoped>
+.top-left {
+  @apply top-10 left-10;
+}
+.top-right {
+  @apply top-10 right-10;
+}
+.bottom-left {
+  @apply bottom-10 left-10;
+}
+.bottom-right {
+  @apply bottom-10 right-10;
+}
+</style>
