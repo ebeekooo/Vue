b14caee20582d6a777e24711f24a53294caad57e
Edisbel Camargo <lebside2701@gmail.com>
2026-04-11 22:16:14 -0500

    Corriegiendo rutas en el sideMenu

--- 06-projects-app/src/modules/projects/components/SideMenu.vue
+++ 06-projects-app/src/modules/projects/components/SideMenu.vue
@@ -11,20 +11,20 @@
         <template v-if="project.tasks.length > 0">
           <details>
             <summary>
-              <RouterLink :to="`/project/${project.id}`">
+              <RouterLink :to="`/projects/${project.id}`">
                 {{ project.name }}
               </RouterLink>
             </summary>
             <ul>
               <li v-for="task in project.tasks" :key="task.id">
-                <RouterLink :to="`/project/${project.id}`">{{ task.name }}</RouterLink>
+                <RouterLink :to="`/projects/${project.id}`">{{ task.name }}</RouterLink>
               </li>
             </ul>
           </details>
         </template>
 
         <template v-else>
-          <RouterLink :to="`/project/${project.id}`">
+          <RouterLink :to="`/projects/${project.id}`">
             {{ project.name }}
           </RouterLink>
         </template>
