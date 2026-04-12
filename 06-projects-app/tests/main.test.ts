aac31d33f4331d6320cabbfd15123add5e0401fe
Edisbel Camargo <lebside2701@gmail.com>
2026-04-12 15:55:38 -0500

    Completando el testing a main.ts

--- 06-projects-app/tests/main.test.ts
+++ 06-projects-app/tests/main.test.ts
@@ -1,3 +1,16 @@
+vi.mock('@/router', () => ({
+  default: 'router',
+}));
+
+vi.mock('pinia', async (importOriginal) => {
+  const mod: any = await importOriginal();
+
+  return {
+    ...mod,
+    createPinia: vi.fn().mockReturnValue('pinia'),
+  };
+});
+
 describe('Main.st', () => {
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   const vue = require('vue');
@@ -11,4 +24,14 @@ describe('Main.st', () => {
   });
 
   vue.createApp = createApp;
+  test('should be configured with pinia and router', async () => {
+    await import('@/main');
+
+    expect(vue.createApp).toHaveBeenCalled();
+    expect(mountSpy).toHaveBeenCalledWith('#app');
+
+    // console.log(useSpy.mock.calls);
+    expect(useSpy).toHaveBeenCalledWith('router');
+    expect(useSpy).toHaveBeenCalledWith('pinia');
+  });
 });
