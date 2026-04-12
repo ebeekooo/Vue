cc2d35f4409245d8afcea55f0457f4447c010e5f
Edisbel Camargo <lebside2701@gmail.com>
2026-04-12 16:00:43 -0500

    Agregando testing a la posicion por defecto del favBotton

---
+++ 06-projects-app/tests/modules/common/components/FavBotton.test.ts
@@ -0,0 +1,15 @@
+import { shallowMount } from '@vue/test-utils';
+import FavBotton from '@/modules/common/components/FavBotton.vue';
+
+describe('<FavBotton/>', () => {
+  test('renders with default position', () => {
+    const wrapper = shallowMount(FavBotton);
+
+    // console.log(wrapper.html());
+    expect(wrapper.props().position).toBe('bottom-right');
+    const buttonClasses = wrapper.find('button').classes();
+    const classesToHave = ['btn', 'btn-circle', 'btn-secondary', 'fixed', 'bottom-right'];
+
+    expect(buttonClasses).toEqual(classesToHave);
+  });
+});
