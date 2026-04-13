75d1edc39118ec69b3cbe9e561648cf3cd89848e
Edisbel Camargo <lebside2701@gmail.com>
2026-04-13 15:23:18 -0500

    Arreglando readme

--- 07-admin-shop/src/modules/shop/views/HomeView.vue
+++ 07-admin-shop/src/modules/shop/views/HomeView.vue
@@ -99,39 +99,37 @@
 
   <ButtonPagination :has-more-data="!!products && products.length < 10" :page="page" />
 </template>
-// //
+
 <script lang="ts" setup>
-//import { getProductsAction } from '@/modules/products/actions';
-//import ButtonPagination from '@/modules/common/components/ButtonPagination.vue';
-//import ProductList from '@/modules/products/components/ProductList.vue';
-//import { useQuery, useQueryClient } from '@tanstack/vue-query';
-//import { useRoute } from 'vue-router';
-//import { ref, watch, watchEffect } from 'vue';
-//
-//const route = useRoute();
-//const page = ref(Number(route.query.page || 1));
-//const queryClient = useQueryClient();
-//
-//const { data: products = [] } = useQuery({
-//  queryKey: ['products', { page: page }],
-//  queryFn: () => getProductsAction(page.value),
-//});
-//
-//watch(
-//  () => route.query.page,
-//  (newPage) => {
-//    page.value = Number(newPage || 1);
-//
-//    window.scrollTo({ top: 0, behavior: 'smooth' });
-//  },
-//);
-//
-//watchEffect(() => {
-//  queryClient.prefetchQuery({
-//    queryKey: ['products', { page: page.value + 1 }],
-//    queryFn: () => getProductsAction(page.value + 1),
-//  });
-//});
-//
+import { getProductsAction } from '@/modules/products/actions';
+import ButtonPagination from '@/modules/common/components/ButtonPagination.vue';
+import ProductList from '@/modules/products/components/ProductList.vue';
+import { useQuery, useQueryClient } from '@tanstack/vue-query';
+import { useRoute } from 'vue-router';
+import { ref, watch, watchEffect } from 'vue';
+
+const route = useRoute();
+const page = ref(Number(route.query.page || 1));
+const queryClient = useQueryClient();
+
+const { data: products = [] } = useQuery({
+  queryKey: ['products', { page: page }],
+  queryFn: () => getProductsAction(page.value),
+});
+
+watch(
+  () => route.query.page,
+  (newPage) => {
+    page.value = Number(newPage || 1);
+
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  },
+);
+
+watchEffect(() => {
+  queryClient.prefetchQuery({
+    queryKey: ['products', { page: page.value + 1 }],
+    queryFn: () => getProductsAction(page.value + 1),
+  });
+});
 </script>
-//
